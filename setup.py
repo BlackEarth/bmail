8293532f02bafe5024c5577a54dc5587b6b47185
Sean Harrison <sah@blackearth.us>
2016-06-14 15:33:29 -0500

    no message

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 
 config = {
   "name": "bmail",
-  "version": "0.2.3",
+  "version": "0.2.4",
   "description": "Email library",
   "url": "https://github.com/BlackEarth/bmail",
   "author": "Sean Harrison",
